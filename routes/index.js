98f56d4c5c8191ed88e2252db27fb69382465c89
eamahma <eamahma@gmail.com>
2021-08-13 15:42:27 -0400

    added comments

--- routes/index.js
+++ routes/index.js
@@ -2,26 +2,41 @@ const notes = require('express').Router();
 const fs = require('fs');
 const { v4 : uuidv4 } = require('uuid');
 
+// read all notes
 notes.get('/notes', (req, res) => {
+    // read existing database content
     const data = JSON.parse(fs.readFileSync('db/db.json'));
+    // send back complete data to client
     res.json(data);
 })
 
+// write new note to database
 notes.post('/notes', (req, res) => {
-    const note_id = req.body;
-    note_id.id = uuidv4();
+    // read content and assign to note 
+    const note = req.body;
+    // assign new id
+    note.id = uuidv4();
+    // read existing database content
     const data = JSON.parse(fs.readFileSync('db/db.json'));
-    data.push(note_id);
+    // add new not to existing data
+    data.push(note);
+    // write updated data back to database
     fs.writeFileSync('db/db.json', JSON.stringify(data));
+    // send back complete data to client
     res.json(data);
 })
 
 notes.delete('/notes/:id', (req, res) => {
+    // read existing database content
     const data = JSON.parse(fs.readFileSync('db/db.json'));
+    // extract note_id from entered parameter id
     const note_id = req.params.id;
+    // create a new data excluding the one match entered id
     const delete_note = data.filter(note => (note.id !== note_id));
+    // write to db.json as new file
     fs.writeFileSync('db/db.json', JSON.stringify(delete_note));
-    res.json(data);
+   // send back complete data to client
+   res.json(data);
 })
 
 module.exports = notes;
--- server.js
+++ server.js
@@ -6,19 +6,25 @@ const PORT = process.env.PORT || 3001;
 
 const app = express();
 
+// Middleware for parsing JSON and urlencoded form data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('/api', api);
 
 app.use(express.static('public'));
 
+// GET Route for homepage
 app.get('/', (req, res) => 
     res.sendFile(path.join(__dirname, '/public/index.html'))
 );
 
+// GET Route for notes page
 app.get('/notes', (req, res) => 
     res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
+
+// Wildcard route to direct users to a 404 page
+app.get('*', (req, res) => res.status(404).send('Path not found!'));
 
 app.listen(PORT, () => 
     console.log(`App listening at http://localhost:${PORT}`)
