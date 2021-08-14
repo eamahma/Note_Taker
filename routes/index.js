446197151e1fb66eefdf98ff8e19ce58963ad23d
eamahma <eamahma@gmail.com>
2021-08-14 15:11:01 -0400

    added router

--- routes/index.js
+++ routes/index.js
@@ -1,42 +1,12 @@
-const notes = require('express').Router();
-const fs = require('fs');
-const { v4 : uuidv4 } = require('uuid');
+const express = require('express');
 
-// read all notes
-notes.get('/notes', (req, res) => {
-    // read existing database content
-    const data = JSON.parse(fs.readFileSync('db/db.json'));
-    // send back complete data to client
-    res.json(data);
-})
+// Import our modular routers for /notes
+const notesRouter = require('./notes');
 
-// write new note to database
-notes.post('/notes', (req, res) => {
-    // read content and assign to note 
-    const note = req.body;
-    // assign new id
-    note.id = uuidv4();
-    // read existing database content
-    const data = JSON.parse(fs.readFileSync('db/db.json'));
-    // add new not to existing data
-    data.push(note);
-    // write updated data back to database
-    fs.writeFileSync('db/db.json', JSON.stringify(data));
-    // send back complete data to client
-    res.json(data);
-})
+const app = express();
 
-notes.delete('/notes/:id', (req, res) => {
-    // read existing database content
-    const data = JSON.parse(fs.readFileSync('db/db.json'));
-    // extract note_id from entered parameter id
-    const note_id = req.params.id;
-    // create a new data excluding the one match entered id
-    const delete_note = data.filter(note => (note.id !== note_id));
-    // write to db.json as new file
-    fs.writeFileSync('db/db.json', JSON.stringify(delete_note));
-   // send back complete data to client
-   res.json(data);
-})
+// Router for /notes
+app.use('/notes', notesRouter);
 
-module.exports = notes;
+module.exports = app;
+
